4706b62cb9edad39d84b1e23b3621aa9bd86b54a
kevncheng <37379325+kevncheng@users.noreply.github.com>
2019-07-31 14:03:33 -0400

    Added Board and Post route

--- client/src/App.js
+++ client/src/App.js
@@ -7,10 +7,15 @@ import LandingPage from "./pages/Landing";
 
 import "./App.css";
 
+import PostDialog from './components/Dialog/PostDialog/PostDialog'
+import BoardDialog from "./components/Dialog/BoardDialog/BoardDialog";
+
 function App() {
   return (
     <MuiThemeProvider theme={theme}>
       <BrowserRouter>
+        <Route path = "/profile/:username/post/create" component = {PostDialog}/>
+        <Route path = "/profile/:username/board/create" component = {BoardDialog}/>
         <Route path="/" component={LandingPage} />
       </BrowserRouter>
     </MuiThemeProvider>
