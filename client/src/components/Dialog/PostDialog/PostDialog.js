85f0a8b946fbb126d3c3fde08b556c4b6fa3cc3e
kevncheng <37379325+kevncheng@users.noreply.github.com>
2019-08-02 01:35:36 -0400

    POST request front to back end

--- client/src/components/Dialog/PostDialog/PostDialog.js
+++ client/src/components/Dialog/PostDialog/PostDialog.js
@@ -5,11 +5,14 @@ import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import { Chip } from '@material-ui/core';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 import _ from 'lodash';
+import axios from 'axios';
 
 import FormContent from './FormContent';
 import FileUploader from './FileUploader';
+import BoardList from './BoardList';
 
 class PostDialog extends React.Component {
     state = {
@@ -19,11 +22,31 @@ class PostDialog extends React.Component {
         tag: '',
         tags: [],
         tagError: '',
-        titleError: ''
+        titleError: '',
+        selectedBoard: '',
+        files: []
+    };
+
+    componentDidMount = async () => {
+        try {
+            const username = this.props.location.pathname.split('/')[2];
+            let res = await axios.get(`/users/${username}`);
+            if (res.data.user.boards) {
+                return this.setState({ boards: res.data.user.boards });
+            }
+        } catch (err) {
+            console.log('Something went wrong with fetching user API');
+        }
     };
 
     onChangeText = e => {
         this.setState({ [e.target.id]: e.target.value });
+    };
+
+    //Board
+    handleSelectChange = e => {
+        this.setState({ selectedBoard: e.target.value });
+        console.log(this.state.selectedBoard);
     };
 
     // Create a tag
@@ -56,33 +79,58 @@ class PostDialog extends React.Component {
 
     // File pond , handle image upload
     onUploadImages = fileItems => {
-        this.setState({
-            files: fileItems.filter(fileItem => {
-                if (fileItem.file.type.toString() === 'image/jpeg') {
-                    return fileItem.file;
-                }
-                return false;
-            })
-        });
-        console.log(this.state.files);
+        let filterFiles = fileItems.filter(
+            fileItem => fileItem.file.type.toString() === 'image/jpeg'
+        );
+        this.setState({ files: filterFiles.map(filterFile => filterFile.file) });
     };
 
     // Create post
-    onCreatePress = () => {
+    onCreatePress = async () => {
+        const username = this.props.location.pathname.split('/')[2];
+        const { title, tags, files = '', description, destination } = this.state;
         if (this.state.title.length < 3) {
             this.setState({ titleError: 'Title must be greater than 3 characters' });
         } else {
-            this.setState({ titleError: '' });
-            this.props.history.push('/');
+            try {
+                const formData = new FormData();
+                formData.append('title', title);
+                formData.append('tags', tags);
+                formData.append('description', description);
+                formData.append('link', destination);
+                formData.append('image', files[0]);
+                await axios({
+                    url: `/users/${username}/posts`,
+                    method: 'POST',
+                    data: formData,
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                });
+
+                this.props.history.push(`/profile/${username}`);
+            } catch (err) {
+                console.log('Something went wrong with creating a post');
+            }
         }
     };
 
     render() {
+        if (!Boolean(this.state.boards)) {
+            return <CircularProgress color='secondary' />;
+        }
         return (
             <>
                 <Dialog open={true} onClose={this.handleClose} aria-labelledby='form-dialog-title'>
-                    <DialogTitle style = {{textAlign:'center'}} id='form-dialog-title'>Create a post</DialogTitle>
+                    <DialogTitle style={{ textAlign: 'center' }} id='form-dialog-title'>
+                        Create a post
+                    </DialogTitle>
                     <DialogContent>
+                        <BoardList
+                            boards={this.state.boards}
+                            handleSelect={this.handleSelectChange}
+                            value={this.state.selectedBoard}
+                        />
                         <FormContent
                             onChangeText={this.onChangeText}
                             titleError={this.state.titleError}
@@ -90,7 +138,7 @@ class PostDialog extends React.Component {
                             onSubmitPress={this.onSubmitPress}
                             tag={this.state.tag}
                             title={this.state.title}
-                            description = {this.state.description}
+                            description={this.state.description}
                             destination={this.state.destination}
                         />
                         {this.renderTags()}
