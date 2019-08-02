27cf4aea06dd03ac8c7e7bf1edd852751bf2237a
kevncheng <37379325+kevncheng@users.noreply.github.com>
2019-08-02 14:46:03 -0400

    Created Navbar Component

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/styles';
+import face from '../../pages/face.jpg';
+
+const useStyles = makeStyles(theme => ({
+    header: {
+        display: 'grid',
+        gridTemplateColumns: '5fr 3fr 6fr 2.5fr 2.5fr 0fr 3fr',
+        minHeight: '20vh',
+        justifyItems: 'center',
+        alignItems: 'center'
+    },
+    cornerIcon: {
+        width: '50px',
+        height: '50px',
+        borderRadius: '50px'
+    },
+    headerSearch: {
+        width: '35vw',
+        minHeight: '40px',
+        borderRadius: '50px',
+        border: '1px solid lightgrey',
+        paddingLeft: '15px'
+    },
+    headerBottomBorder: {
+        minHeight: '5px',
+        backgroundImage: 'linear-gradient(lightgrey, white)'
+    },
+    headerContainer: {
+        position: 'fixed',
+        background: 'white',
+        zIndex: '10'
+    },
+    placeholderHeader: {
+        minHeight: '20vh',
+        height: '20vh'
+    }
+}));
+
+const Profile = () => {
+    const style = useStyles();
+    return (
+        <div>
+            <div className={style.headerContainer}>
+                <div className={style.header}>
+                    <div>
+                        <h3>Dream Home</h3>
+                    </div>
+                    <div />
+                    <div>
+                        <input className={style.headerSearch} placeholder='Search' />
+                    </div>
+                    <div>
+                        <h5>Home</h5>
+                    </div>
+                    <div>
+                        <h5>Following</h5>
+                    </div>
+                    <div />
+                    <img className={style.cornerIcon} src={face} alt='' />
+                </div>
+                <div className={style.headerBottomBorder} />
+            </div>
+            <div className={style.placeholderHeader}>placeholder</div>
+        </div>
+    );
+};
+
+export default Profile;
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,72 +1,40 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/styles';
 import face from './face.jpg';
-import {Card, Typography} from '@material-ui/core';
+import { Card, Typography } from '@material-ui/core';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import house from './house.png'
-import {Link} from 'react-router-dom'
-
+import house from './house.png';
+import { Link } from 'react-router-dom';
+import Navbar from '../components/Navbar/Navbar';
 const useStyles = makeStyles(theme => ({
-    header: {
-        display: 'grid',
-        gridTemplateColumns: '5fr 3fr 6fr 2.5fr 2.5fr 0fr 3fr',
-        minHeight: '20vh',
-        justifyItems: 'center',
-        alignItems: 'center',
-    },
-    cornerIcon: {
-        width: '50px',
-        height: '50px',
-        borderRadius: '50px',
-    },
-    headerSearch: {
-        width: '35vw',
-        minHeight: '40px',
-        borderRadius: '50px',
-        border: '1px solid lightgrey',
-        paddingLeft: '15px'
-    },
-    headerBottomBorder: {
-        minHeight: '5px',
-        backgroundImage: 'linear-gradient(lightgrey, white)'
-    },
-    headerContainer: {
-        position: 'fixed',
-        background: 'white',
-        zIndex: '10',
-    },
-    placeholderHeader: {
-        minHeight: '20vh',
-        height: '20vh',
-    },
     subHeader: {
         minHeight: '30vh',
         display: 'grid',
         gridTemplateColumns: '5fr 2fr 5fr',
         alignItems: 'center',
         justifyItems: 'center',
-        borderBottom: '1px solid lightgrey',
+        borderBottom: '1px solid lightgrey'
     },
     subHeaderIcon: {
         width: '100px',
         height: '100px',
-        borderRadius: '50px',
+        borderRadius: '50px'
     },
     nameContainer: {
         display: 'grid',
         gridTemplateColumns: '1fr 1fr',
         justifyItems: 'center',
-        alignItems: 'center',
+        alignItems: 'center'
     },
     profileName: {
         margin: '0',
-        padding:'0',
+        padding: '0'
     },
     profileFollowers: {
         margin: '0',
-        padding:'0',
-        paddingTop: '5px',
+        padding: '0',
+        paddingTop: '5px'
     },
     createBoard: {
         background: 'white',
@@ -74,7 +42,7 @@ const useStyles = makeStyles(theme => ({
         borderRadius: '25px',
         padding: '10px 20px 10px 20px',
         fontWeight: 'bold',
-        marginRight: '10px',
+        marginRight: '10px'
     },
     createPost: {
         background: 'black',
@@ -82,14 +50,14 @@ const useStyles = makeStyles(theme => ({
         border: 'none',
         borderRadius: '25px',
         padding: '10px 20px 10px 20px',
-        fontWeight: 'bold',
+        fontWeight: 'bold'
     },
     tabSection: {
         minHeight: '25vh',
         display: 'grid',
         gridTemplateColumns: '1fr 1.9fr',
         alignItems: 'center',
-        justifyItems: 'center',
+        justifyItems: 'center'
     },
     tab: {
         background: 'white',
@@ -97,7 +65,7 @@ const useStyles = makeStyles(theme => ({
         borderRadius: '25px',
         padding: '10px 20px 10px 20px',
         fontWeight: 'bold',
-        marginRight: '10px',
+        marginRight: '10px'
     },
     activeTab: {
         background: 'black',
@@ -106,149 +74,145 @@ const useStyles = makeStyles(theme => ({
         borderRadius: '25px',
         padding: '10px 20px 10px 20px',
         fontWeight: 'bold',
-        marginRight: '10px',
+        marginRight: '10px'
     },
     activePanel: {
         display: 'grid',
         justifyItems: 'center',
-        marginBottom: ' 50px',
+        marginBottom: ' 50px'
     },
     gridContainer: {
         display: 'grid',
         gridTemplateColumns: '1fr 1fr 1fr',
         justifyItems: 'center',
-        gridGap: '20px',
+        gridGap: '20px'
     },
     card: {
         height: '40vh',
-        width: '26vw',
+        width: '26vw'
     },
     cardImg: {
-        height: '31vh',
+        height: '31vh'
     },
     cardHeader: {
-        marginLeft: '20px',
-    },
-}))
+        marginLeft: '20px'
+    }
+}));
 
 const Profile = () => {
-    const style =  useStyles()
-    let [activePanel, toggle] = useState('board')
+    const style = useStyles();
+    let [activePanel, toggle] = useState('board');
     return (
         <div>
-            <div className={style.headerContainer}>
-                <div className={style.header}>
-                    <div>
-                        <h3>Dream Home</h3>
-                    </div>
-                    <div>
-                    </div>
-                    <div>
-                        <input className={style.headerSearch} placeholder="Search"></input>
-                    </div>
-                    <div>
-                        <h5>Home</h5>
-                    </div>
-                    <div>
-                        <h5>Following</h5>
-                    </div>
-                    <div>
-                    </div>
-                    <img className={style.cornerIcon} src={face} alt='' />
-                </div>
-                <div className={style.headerBottomBorder}>
-                </div>
-            </div>
-            <div className={style.placeholderHeader}>
-                placeholder
-            </div>
+            <Navbar/>
             <div className={style.subHeader}>
                 <div className={style.nameContainer}>
-                    <img src={face} alt='' className={style.subHeaderIcon}/>
+                    <img src={face} alt='' className={style.subHeaderIcon} />
                     <div>
                         <h3 className={style.profileName}>Delores Jones</h3>
                         <h5 className={style.profileFollowers}>134 Followers | 280 Following</h5>
                     </div>
                 </div>
+                <div />
                 <div>
-                </div>
-                <div>
-                    <Link to='/profile/:username/board/create'><button className={style.createBoard}>Create Board</button></Link>
-                    <Link to='/profile/:username/post/create'><button className={style.createPost}>Create Post</button></Link>
+                    <Link to='/profile/:username/board/create'>
+                        <button className={style.createBoard}>Create Board</button>
+                    </Link>
+                    <Link to='/profile/:username/post/create'>
+                        <button className={style.createPost}>Create Post</button>
+                    </Link>
                 </div>
             </div>
-            <div style={{display: activePanel === 'board' ? 'grid' : 'none'}}>
+            <div style={{ display: activePanel === 'board' ? 'grid' : 'none' }}>
                 <div className={style.tabSection}>
                     <div>
-                        <button className={style.activeTab} onClick={() => toggle(activePanel = 'board')}>Boards</button>
-                        <button className={style.tab} onClick={() => toggle(activePanel = 'post')}>My Posts</button>
+                        <button
+                            className={style.activeTab}
+                            onClick={() => toggle((activePanel = 'board'))}
+                        >
+                            Boards
+                        </button>
+                        <button
+                            className={style.tab}
+                            onClick={() => toggle((activePanel = 'post'))}
+                        >
+                            My Posts
+                        </button>
                     </div>
-                    <div>
-                    </div>
-                </div>
-                <div className={style.activePanel}>
-                    <div className={style.gridContainer}>
-                    <Card className={style.card}>
-                        <CardActionArea className={style.card}>
-                            <CardMedia className={style.cardImg} image={house}/>
-                            <Typography variant='h6' className={style.cardHeader}>
-                                House
-                            </Typography>
-                            <Typography variant='p' className={style.cardHeader}>
-                                80 posts
-                            </Typography>
-                        </CardActionArea>
-                    </Card>
-                    <Card className={style.card}>
-                        <CardActionArea className={style.card}>
-                            <CardMedia className={style.cardImg} image={house}/>
-                            <Typography variant='h6' className={style.cardHeader}>
-                                House
-                            </Typography>
-                            <Typography variant='p' className={style.cardHeader}>
-                                80 posts
-                            </Typography>
-                        </CardActionArea>
-                    </Card>
-                    <Card className={style.card}>
-                        <CardActionArea className={style.card}>
-                            <CardMedia className={style.cardImg} image={house}/>
-                            <Typography variant='h6' className={style.cardHeader}>
-                                House
-                            </Typography>
-                            <Typography variant='p' className={style.cardHeader}>
-                                80 posts
-                            </Typography>
-                        </CardActionArea>
-                    </Card>
-                    <Card className={style.card}>
-                        <CardActionArea className={style.card}>
-                            <CardMedia className={style.cardImg} image={house}/>
-                            <Typography variant='h6' className={style.cardHeader}>
-                                House
-                            </Typography>
-                            <Typography variant='p' className={style.cardHeader}>
-                                80 posts
-                            </Typography>
-                        </CardActionArea>
-                    </Card>
-                    </div>
-                </div>
-            </div>
-            <div style={{display: activePanel === 'post' ? 'grid' : 'none'}}>
-                <div className={style.tabSection}>
-                    <div>
-                        <button className={style.tab} onClick={() => toggle(activePanel = 'board')}>Boards</button>
-                        <button className={style.activeTab} onClick={() => toggle(activePanel = 'post')}>My Posts</button>
-                    </div>
-                    <div>
-                    </div>
+                    <div />
                 </div>
                 <div className={style.activePanel}>
                     <div className={style.gridContainer}>
                         <Card className={style.card}>
                             <CardActionArea className={style.card}>
-                                <CardMedia className={style.cardImg} image={house}/>
+                                <CardMedia className={style.cardImg} image={house} />
+                                <Typography variant='h6' className={style.cardHeader}>
+                                    House
+                                </Typography>
+                                <Typography variant='p' className={style.cardHeader}>
+                                    80 posts
+                                </Typography>
+                            </CardActionArea>
+                        </Card>
+                        <Card className={style.card}>
+                            <CardActionArea className={style.card}>
+                                <CardMedia className={style.cardImg} image={house} />
+                                <Typography variant='h6' className={style.cardHeader}>
+                                    House
+                                </Typography>
+                                <Typography variant='p' className={style.cardHeader}>
+                                    80 posts
+                                </Typography>
+                            </CardActionArea>
+                        </Card>
+                        <Card className={style.card}>
+                            <CardActionArea className={style.card}>
+                                <CardMedia className={style.cardImg} image={house} />
+                                <Typography variant='h6' className={style.cardHeader}>
+                                    House
+                                </Typography>
+                                <Typography variant='p' className={style.cardHeader}>
+                                    80 posts
+                                </Typography>
+                            </CardActionArea>
+                        </Card>
+                        <Card className={style.card}>
+                            <CardActionArea className={style.card}>
+                                <CardMedia className={style.cardImg} image={house} />
+                                <Typography variant='h6' className={style.cardHeader}>
+                                    House
+                                </Typography>
+                                <Typography variant='p' className={style.cardHeader}>
+                                    80 posts
+                                </Typography>
+                            </CardActionArea>
+                        </Card>
+                    </div>
+                </div>
+            </div>
+            <div style={{ display: activePanel === 'post' ? 'grid' : 'none' }}>
+                <div className={style.tabSection}>
+                    <div>
+                        <button
+                            className={style.tab}
+                            onClick={() => toggle((activePanel = 'board'))}
+                        >
+                            Boards
+                        </button>
+                        <button
+                            className={style.activeTab}
+                            onClick={() => toggle((activePanel = 'post'))}
+                        >
+                            My Posts
+                        </button>
+                    </div>
+                    <div />
+                </div>
+                <div className={style.activePanel}>
+                    <div className={style.gridContainer}>
+                        <Card className={style.card}>
+                            <CardActionArea className={style.card}>
+                                <CardMedia className={style.cardImg} image={house} />
                                 <Typography variant='h6' className={style.cardHeader}>
                                     House
                                 </Typography>
@@ -261,7 +225,7 @@ const Profile = () => {
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Profile;
