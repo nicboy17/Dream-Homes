75b9c877b2fa03fe1577519b1d27ef61e9e30678
mrchrischan <chanchris1117@gmail.com>
2019-08-09 12:56:01 -0400

    deleted not found

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,6 @@ import Home from './pages/Home';
 import SignUp from './pages/SignUp.js';
 import Profile from './pages/Profile.js';
 
-import NotFound from './pages/NotFound.js';
 import PostPage from './pages/Post/PostPage';
 
 function App () {
