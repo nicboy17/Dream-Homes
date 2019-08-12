938066bad4bfeb19b5a869533749a15b0fe56234
kevncheng <37379325+kevncheng@users.noreply.github.com>
2019-08-12 11:45:43 -0400

    Use .env for JWT secret

--- server/src/middleware/token.js
+++ server/src/middleware/token.js
@@ -1,12 +1,11 @@
 const jwt = require('jsonwebtoken');
-const secret = 'secret';
 
 module.exports = function () {
     return function (req, res, next) {
         const token = req.body.token || req.body.query || req.headers['access-token']; // Check for token in body, URL, or headers
 
         if (token) {
-            jwt.verify(token, secret, function (err, decoded) {
+            jwt.verify(token, process.env.SECRET, function (err, decoded) {
                 if (err) {
                     res.status(403).json({ success: false, message: 'Token invalid' });
                 } else {
