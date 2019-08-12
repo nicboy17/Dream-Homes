a1a91b4fcbd2811c86dee4f1a19b0e5b234215f8
kevncheng <37379325+kevncheng@users.noreply.github.com>
2019-08-12 11:10:02 -0400

    Changed 404 to 401 status

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -102,7 +102,7 @@ router.put('/board/:id', async (req,res) => {
     try {
         const board = await Board.findById(req.params.id);
         if(board.user.toString() !== req.decoded._id.toString()) {
-            return res.status(404).json({msg: 'You do not have the authorization to add to this board'});
+            return res.status(401).json({msg: 'You do not have the authorization to add to this board'});
         }
         // Check to see if there is a board with that id
         if(!board) {
