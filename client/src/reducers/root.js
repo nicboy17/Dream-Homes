e2b7ec6169eff5bfc920d9ca59170cb634115be2
kevncheng <37379325+kevncheng@users.noreply.github.com>
2019-08-13 19:29:04 -0400

    Added fetch post action and reducer

--- client/src/reducers/root.js
+++ client/src/reducers/root.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import UserStore from './user';
 import PostStore from './post';
+import posts from './postFetch';
 
 const root = combineReducers({
     UserStore,
-    PostStore
+    PostStore,
+    posts
 });
 export default root;
