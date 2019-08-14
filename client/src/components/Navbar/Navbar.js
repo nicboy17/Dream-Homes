08daa0b37e9d56dec0c0544da06f90055c78fd49
mrchrischan <chanchris1117@gmail.com>
2019-08-14 17:45:01 -0400

    following users UI
    added state tracker for seeing if the user is followed or not
    created button dependant on state

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -12,7 +12,7 @@ const useStyles = makeStyles(theme => ({
     header: {
         display: 'grid',
         gridTemplateColumns: '5fr 3fr 6fr 2.5fr 2.5fr 0fr 3fr',
-        minHeight: '6rem',
+        minHeight: '16vh',
         justifyItems: 'center',
         alignItems: 'center'
     },
@@ -38,8 +38,8 @@ const useStyles = makeStyles(theme => ({
         zIndex: '10'
     },
     placeholderHeader: {
-        minHeight: '14vh',
-        height: '14vh'
+        minHeight: '16vh',
+        height: '16vh'
     },
     menu: {
         marginTop: '4rem'
