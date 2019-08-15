223cbba95c032be5368979be84f8adf1bfc94663
kevncheng <37379325+kevncheng@users.noreply.github.com>
2019-08-15 10:39:18 -0400

    Minor update

--- client/src/reducers/profileReducer.js
+++ client/src/reducers/profileReducer.js
@@ -10,22 +10,22 @@ import {
 const INITIAL_STATE = {};
 
 export default (state = INITIAL_STATE, action) => {
-    const { type, response: { user, board, post }, err } = action;
-    switch (type) {
+    const response = action.response;
+    switch (action.type) {
     case GET_USER_BOARDS_POSTS_SUCCESS:
-        return user;
+        return response.user;
     case GET_USER_BOARDS_POSTS_ERROR:
-        return { ...state, error: err };
+        return { ...state, error: action.err };
     case ADD_BOARD_SUCCESS:
-        state.boards.push(board);
+        state.boards.push(response.board);
         return { ...state };
     case ADD_BOARD_ERROR:
-        return { ...state, error: err };
+        return { ...state, error: action.err };
     case ADD_POST_SUCCESS:
-        state.posts.push(post);
+        state.posts.push(response.post);
         return { ...state };
     case ADD_POST_ERROR:
-        return { ...state, error: err };
+        return { ...state, error: action.err };
     default:
         return state;
     }
