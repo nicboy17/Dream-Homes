efcc9ce66e23267d5826d30a9b2936f2bc944cf3
Nick Major <nmajor@uoguelph.ca>
2019-08-15 23:47:20 -0400

    fix jwt verify

--- server/src/middleware/token.js
+++ server/src/middleware/token.js
@@ -1,11 +1,9 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (token, res) => {
-    jwt.verify (token, process.env.SECRET, (err, decoded) => {
-        if (err) {
-            return res.status (403).json ({ success: false, message: 'Token invalid' });
-        }
-
-        return decoded;
-    });
+    try {
+        return jwt.verify (token, process.env.SECRET);
+    } catch (err) {
+        return res.status (403).json ({ success: false, message: 'Token invalid' });
+    }
 };
