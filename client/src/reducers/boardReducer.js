f4108a4666f2b9e2a20f5e3dd6f5ad9113bce1d5
kevncheng <37379325+kevncheng@users.noreply.github.com>
2019-08-22 10:11:23 -0400

    Post In Board Fix

--- client/src/reducers/boardReducer.js
+++ client/src/reducers/boardReducer.js
@@ -1,7 +1,9 @@
 import { FETCHING_BOARD_POSTS, FETCH_BOARD_POSTS_FAIL, FETCH_BOARD_POSTS_SUCCESS } from '../actions/types';
 
 const INITIAL_STATE = {
-    board: {},
+    board: {
+        posts: []
+    },
     loading: false
 };
 
@@ -9,7 +11,7 @@ export default (state = INITIAL_STATE, action) => {
     const { type, payload } = action;
     switch (type) {
     case FETCHING_BOARD_POSTS:
-        return { ...state, loading: true };
+        return { ...state, loading: true, board: INITIAL_STATE.board };
     case FETCH_BOARD_POSTS_SUCCESS:
         return { ...state, loading: false, board: payload.board };
     case FETCH_BOARD_POSTS_FAIL:
